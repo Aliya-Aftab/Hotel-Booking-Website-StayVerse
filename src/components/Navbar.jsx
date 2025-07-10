0173f4002a20732331c41be899835bdccae5c1af
Aliya Aftab <aftabaliya40@gmail.com>
2025-07-10 21:17:43 +0530

    FrontEnd Completed

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,197 @@
+import React, { useEffect, useState } from "react";
+import { Link, useLocation, useNavigate } from "react-router-dom";
+import { assets } from "../assets/assets";
+import { useClerk, useUser, UserButton } from "@clerk/clerk-react";
+
+const BookIcon = () => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="24"
+    height="24"
+    viewBox="0 0 24 24"
+    fill="none"
+    stroke="currentColor"
+    strokeWidth="2"
+    strokeLinecap="round"
+    strokeLinejoin="round"
+    className="feather feather-book-open"
+  >
+    <path d="M2 3h20a2 2 0 012 2v14a2 2 0 01-2 2H2a2 2 0 01-2-2V5a2 2 0 012-2z"></path>
+    <path d="M22 7H2"></path>
+    <path d="M22 17H2"></path>
+    <path d="M6.5 3h11a1.5 1.5 0 011.5 1.5v15a1.5 1.5 0 01-1.5 1.5h-11a1.5 1.5 0 01-1.5-1.5V4.5A1.5 1.5 0 016.5 3z"></path>
+    <path d="M6.5 7h11"></path>
+    <path d="M6.5 17h11"></path>
+    <path d="M6.5,12h11"></path>
+    <path d="M6.5,8h11"></path>
+  </svg>
+);
+const Navbar = () => {
+  const navLinks = [
+     { name: "StayVerse", path: "/" },
+  { name: "Book a Stay", path: "/rooms" },
+  { name: "Experiences", path: "/" },
+  { name: "Why Choose Us", path: "/" },
+  ];
+
+  const [isScrolled, setIsScrolled] = useState(false);
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const { openSignIn } = useClerk();
+  const { user } = useUser();
+  const navigate = useNavigate();
+  const location = useLocation();
+
+  useEffect(() => {
+    // If the user is NOT on the homepage, always set scrolled to true
+    if (location.pathname !== "/") {
+      setIsScrolled(true);
+      return;
+    } else {
+      setIsScrolled(false);
+    }
+    setIsScrolled((prev) => (location.pathname !== "/" ? true : prev));
+    const handleScroll = () => {
+      setIsScrolled(window.scrollY > 10);
+    };
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, [location.pathname]);
+
+  return (
+    <nav
+      className={`fixed top-0 left-0  w-full flex items-center justify-between px-4 md:px-16 lg:px-24 xl:px-32 transition-all duration-500 z-50 ${
+        isScrolled
+          ? "bg-white/80 shadow-md text-gray-700 backdrop-blur-lg py-3 md:py-4"
+          : "py-4 md:py-6"
+      }`}
+    >
+      {/* Logo */}
+      <Link to="/">
+        <h1 className={`text-2xl font-bold tracking-wide ${isScrolled ? 'text-gray-800' : 'text-white'}`}>
+  StayVerse
+</h1>
+
+      </Link>
+
+      {/* Desktop Nav */}
+      <div className="hidden md:flex items-center gap-4 lg:gap-8">
+        {navLinks.map((link, i) => (
+          <a
+            key={i}
+            href={link.path}
+            className={`group flex flex-col gap-0.5 ${
+              isScrolled ? "text-gray-700" : "text-white"
+            }`}
+          >
+            {link.name}
+            <div
+              className={`${
+                isScrolled ? "bg-gray-700" : "bg-white"
+              } h-0.5 w-0 group-hover:w-full transition-all duration-300`}
+            />
+          </a>
+        ))}
+        <button
+          className={`border px-4 py-1 text-sm font-light rounded-full cursor-pointer ${
+            isScrolled ? "text-black" : "text-white"
+          } transition-all`}
+          onClick={() => navigate("/owner")}
+        >
+          Dashbord
+        </button>
+      </div>
+
+      {/* Desktop Right */}
+      <div className="hidden md:flex items-center gap-4">
+        <img
+          src={assets.searchIcon}
+          alt="search"
+          className={`${isScrolled ? 'invert' : ''} h-7 transition-all duration-500`}
+
+        />
+        {user ? (
+          <UserButton>
+            <UserButton.MenuItems>
+              <UserButton.Action
+                label="My Bookings"
+                labelIcon={<BookIcon />}
+                onClick={() => navigate("/my-bookings")}
+              />
+            </UserButton.MenuItems>
+          </UserButton>
+        ) : (
+          <button
+            onClick={openSignIn}
+            className="bg-black text-white px-8 py-2.5 rounded-full ml-4 transition-all duration-500"
+          >
+            Login
+          </button>
+        )}
+      </div>
+
+      {/* Mobile Menu Button */}
+
+      <div className="flex items-center gap-3 md:hidden">
+        {user && (
+          <UserButton>
+            <UserButton.MenuItems>
+              <UserButton.Action
+                label="My Bookings"
+                labelIcon={<BookIcon />}
+                onClick={() => navigate("/my-bookings")}
+              />
+            </UserButton.MenuItems>
+          </UserButton>
+        )}
+        <img
+          onClick={() => setIsMenuOpen(!isMenuOpen)}
+          src={assets.menuIcon}
+          alt="menuIcon"
+          className={`${isScrolled ? 'invert' : ''} h-4`}
+
+        />
+      </div>
+
+      {/* Mobile Menu */}
+      <div
+        className={`fixed top-0 left-0 w-full h-screen bg-white text-base flex flex-col md:hidden items-center justify-center gap-6 font-medium text-gray-800 transition-all duration-500 ${
+          isMenuOpen ? "translate-x-0" : "-translate-x-full"
+        }`}
+      >
+        <button
+          className="absolute top-4 right-4"
+          onClick={() => setIsMenuOpen(false)}
+        >
+          <img src={assets.closeIcon} alt="closeIcon" className="h-6.5" />
+        </button>
+
+        {navLinks.map((link, i) => (
+          <a key={i} href={link.path} onClick={() => setIsMenuOpen(false)}>
+            {link.name}
+          </a>
+        ))}
+
+        {user && (
+          <button
+            className="border px-4 py-1 text-sm font-light rounded-full cursor-pointer transition-all"
+            onClick={() => navigate("/owner")}
+          >
+            Dashboard
+          </button>
+        )}
+
+        {!user && (
+          <button
+            onClick={openSignIn}
+            className="bg-black text-white px-8 py-2.5 rounded-full transition-all duration-500"
+          >
+            Login
+          </button>
+        )}
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
